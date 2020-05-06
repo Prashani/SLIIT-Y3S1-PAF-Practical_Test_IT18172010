92deee052446fee63c4c4b0170cff993bca4380e
Prashani <aruniprashani@gmail.com>
2020-05-06 17:39:00 +0530

    Implemented deletePayment and other reqyuired methods in payments

--- src/Payment.java
+++ src/Payment.java
@@ -181,5 +181,150 @@ public class Payment {
 
 	}
 
+	public List<PaymentModel> readPaymentsUser(String id, String user) {
+		String userType = "";
+		List<PaymentModel> payementesList = new ArrayList<>();
+		switch (user) {
+		case "patient":
+			userType = "patientID";
+			break;
+		case "hospital":
+			userType = "hospitalID";
+			break;
+		case "doctor":
+			userType = "doctorID";
+			break;
+
+		default:
+			break;
+		}
+
+		try {
+			Connection con = connect();
+			if (con == null) {
+//				return "Error while connecting to the database for reading.";
+			}
+
+			String query = "select * from payments where " + userType + " = " + id;
+
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String paymentID = Integer.toString(rs.getInt("id"));
+				String patientID = Integer.toString(rs.getInt("patientID"));
+				String hospitalID = Integer.toString(rs.getInt("hospitalID"));
+				String doctorID = Integer.toString(rs.getInt("doctorID"));
+				String total = rs.getString("total");
+				String dates = rs.getString("date");
+
+				PaymentModel paymodel = new PaymentModel();
+				paymodel.setDates(dates);
+				paymodel.setDoctorID(doctorID);
+				paymodel.setHospitalID(hospitalID);
+				paymodel.setPatientID(patientID);
+				paymodel.setPaymentID(paymentID);
+				paymodel.setTotal(total);
+
+				payementesList.add(paymodel);
+
+			}
+
+		} catch (Exception e) {
+
+			System.err.println(e.getMessage());
+		}
+		return payementesList;
+	}
+
+	public String deleteItem(String paymentID) {
+
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			String query = "delete from payments where id=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			preparedStmt.setInt(1, Integer.parseInt(paymentID));
+			preparedStmt.execute();
+			con.close();
+			String newItems = readPayments();
+			output = "{\"status\":\"success\", \"data\": \"" + newItems + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\": \"Error while inserting the item.\"}";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String updatePaymentDetails(String cardNo, String cvv, String patientID, int ppymentDetailsId) {
+
+		String output = "";
+
+		try {
+
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+
+			String query = "UPDATE paymentDetails SET cardNo= ? ,cvv=?,patientID=? WHERE idpaymentDetails=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			preparedStmt.setString(1, cardNo);
+			preparedStmt.setString(2, cvv);
+			preparedStmt.setString(3, patientID);
+			preparedStmt.setInt(4, ppymentDetailsId);
+
+			preparedStmt.execute();
+
+			con.close();
+			output = "Updated successfully";
+
+		} catch (Exception e) {
+			output = "Error while updating the item.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public PaymentDetailsModel readPaymentsDetailsUser(String patientsID) {
+		PaymentDetailsModel details = new PaymentDetailsModel();
+
+		try {
+			Connection con = connect();
+			if (con == null) {
+
+//				return "Error while connecting to the database for reading.";
+			}
+
+			String query = "select * from paymentDetails where patientID = " + patientsID;
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+
+				String cvv = rs.getString("cvv");
+				String cardNo = rs.getString("cardNo");
+				int idpaymentDetails = rs.getInt("idpaymentDetails");
+				// String idpaymentDetails = "sfdf";
+				String patientID = rs.getString("patientID");
+
+				details.setCvv(cvv);
+				details.setCardNo(cardNo);
+				details.setPatientID(patientID);
+				details.setPpymentDetailsId(idpaymentDetails);
+
+			}
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage() + "---------------------------------------------");
+		}
+
+		return details;
+	}
 
 }
