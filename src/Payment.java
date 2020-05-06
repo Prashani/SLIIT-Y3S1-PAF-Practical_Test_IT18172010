bd3dc2a671fb330f72526b154f866c740b189e66
Prashani <aruniprashani@gmail.com>
2020-05-06 17:33:15 +0530

    Added database connection

---
+++ src/Payment.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.print.attribute.standard.MediaSize.Other;
+
+public class Payment {
+
+//	PaymentModel paymentmodel;
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sliitpafs", "root", "root");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	
+
+}
